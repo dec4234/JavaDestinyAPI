6ffd71e979fcc6d49f9bc18d1cd238a6555ceb8c
dec4234 <decarround04@gmail.com>
2021-05-25 10:42:28 -0400

    BungieUser Update

--- src/main/java/material/user/BungieUser.java
+++ src/main/java/material/user/BungieUser.java
@@ -15,18 +15,28 @@ import material.clan.Clan;
 import material.stats.activities.ActivityInfo;
 import utils.HttpUtils;
 import utils.StringUtils;
+import utils.framework.ContentFramework;
 
+import java.text.DecimalFormat;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class BungieUser {
+/**
+ * A "BungieUser" is a Destiny 2 Player.
+ *
+ * It is possible for there to be no information pertaining to a specific BungieUser so
+ * call isValidUser() before anything else.
+ *
+ * Most information for this class is pulled from the /LinkedProfiles/ Endpoint
+ */
+public class BungieUser extends ContentFramework {
 
 	private String bungieMembershipID;
 	private String displayName;
 	private Date lastPlayed;
-	private JsonObject jo, je; // The JsonObject containing all data related to the user
+	private JsonObject je; // The JsonObject of the profile used to get most information
 	private HttpUtils hu = new HttpUtils();
 
 	private ArrayList<Integer> applicableMembershipTypes = new ArrayList<>();
@@ -34,14 +44,45 @@ public class BungieUser {
 	private List<DestinyCharacter> destinyCharacters = null;
 	private int playTime = -1, crossSaveOverride = -1, membershipType = -1;
 	private boolean isPublic, isCrossSavePrimary, isOverridden = false;
+	private int intendedPlatform = -2;
 
 	private Clan clan;
 
+	/**
+	 * The simplest starter of a BungieUser
+	 * Just provide the bungie membership id of the user
+	 *
+	 * @param bungieMembershipID
+	 */
 	public BungieUser(String bungieMembershipID) {
+		super("https://www.bungie.net/Platform/Destiny2/-1/Profile/" + bungieMembershipID + "/LinkedProfiles/?components=200", source -> {
+			return source.getAsJsonObject("Response");
+		});
 		this.bungieMembershipID = bungieMembershipID;
 	}
 
+	/**
+	 * Used to provide a little more context for what profile the BungieUser class should pull info from
+	 * because a LinkedProfiles request can have multiple platform accounts attached
+	 *
+	 * @param bungieMembershipID The bungie Id of the user you want to get information for
+	 * @param intendedPlatform The platform that you want to pull information for
+	 */
+	public BungieUser(String bungieMembershipID, DestinyPlatform intendedPlatform) {
+		super("https://www.bungie.net/Platform/Destiny2/-1/Profile/" + bungieMembershipID + "/LinkedProfiles/?components=200", source -> {
+			return source.getAsJsonObject("Response");
+		});
+		this.bungieMembershipID = bungieMembershipID;
+		this.intendedPlatform = intendedPlatform.getPlatformCode();
+	}
+
+	/**
+	 * A constructor contianing more information which may decrease load times in certain circumstances
+	 */
 	public BungieUser(String bungieMembershipID, String displayName, int crossSaveOverride, int membershipType, boolean isPublic) {
+		super("https://www.bungie.net/Platform/Destiny2/-1/Profile/" + bungieMembershipID + "/LinkedProfiles/?components=200", source -> {
+			return source.getAsJsonObject("Response");
+		});
 		this.bungieMembershipID = bungieMembershipID;
 		this.displayName = displayName;
 		this.crossSaveOverride = crossSaveOverride;
@@ -50,42 +91,30 @@ public class BungieUser {
 	}
 
 	/**
+	 * Gets the bungie membership ID of the user
+	 */
+	public String getBungieMembershipID() { return bungieMembershipID; }
+
+	/**
 	 * Determines if the user has any profiles on their account
 	 * Useful to see if a user's account has any data on it
 	 */
 	public boolean isValidUser() {
 		try {
-			checkJO();
-			return jo.get("Response").getAsJsonObject().get("profiles").getAsJsonArray().size() != 0;
+			return getJO().get("Response").getAsJsonObject().get("profiles").getAsJsonArray().size() != 0;
 		} catch (NullPointerException nullPointerException) {
 			return false;
 		}
 	}
 
 	/**
-	 * Gets the bungie membership ID of the user
-	 */
-	public String getBungieMembershipID() { return bungieMembershipID; }
-
-	/**
 	 * Gets the display name of the user
 	 * Prefers returning the name of their account on steam, if they have one
 	 */
 	public String getDisplayName() {
-		if (displayName == null) {
-			checkJE();
-			displayName = je.get("displayName").getAsString();
-			JsonArray array = jo.getAsJsonObject("Response").getAsJsonArray("profilesWithErrors");
-			if (array != null && array.size() != 0 && je.get("membershipType").getAsInt() != 3) {
-				for (JsonElement je : array) {
-					if (je.getAsJsonObject().getAsJsonObject("infoCard").get("membershipType").getAsInt() == 3) {
-						checkJE();
-						displayName = je.getAsJsonObject().getAsJsonObject("infoCard").get("displayName").getAsString();
-					}
-				}
-			}
+		if(displayName == null) {
+			displayName = getJE().get("displayName").getAsString();
 		}
-
 		return displayName;
 	}
 
@@ -94,60 +123,71 @@ public class BungieUser {
 	 */
 	public Date getLastPlayed() {
 		if (lastPlayed == null) {
-			checkJE();
-			lastPlayed = StringUtils.valueOfZTime(je.get("dateLastPlayed").getAsString());
+			lastPlayed = StringUtils.valueOfZTime(getJE().get("dateLastPlayed").getAsString());
 		}
 		return lastPlayed;
 	}
 
 	/**
-	 * Gets an integer representing the number of days since the user last played
+	 * Gets a double representing the number of days since the user last played
 	 */
 	public double getDaysSinceLastPlayed() {
-		return (double) ChronoUnit.DAYS.between(getLastPlayed().toInstant(), new Date().toInstant());
+		DecimalFormat df = new DecimalFormat("0.##");
+		return Double.parseDouble(df.format((new Date().getTime() - getLastPlayed().getTime()) / 1000.0 / 60.0 / 60.0 / 24.0));
 	}
 
 	public boolean isOverridden() {
-		checkJE();
-		return !isOverridden ? isOverridden = je.get("isOverridden").getAsBoolean() : isOverridden;
+		return !isOverridden ? isOverridden = getJE().get("isOverridden").getAsBoolean() : isOverridden;
 	}
 
 	public boolean isCrossSavePrimary() {
-		checkJE();
-		return !isCrossSavePrimary ? isCrossSavePrimary = je.get("isCrossSavePrimary").getAsBoolean() : isCrossSavePrimary;
+		return !isCrossSavePrimary ? isCrossSavePrimary = getJE().get("isCrossSavePrimary").getAsBoolean() : isCrossSavePrimary;
 	}
 
 	public int getCrossSaveOverride() {
-		checkJE();
-		return crossSaveOverride == -1 ? crossSaveOverride = je.get("crossSaveOverride").getAsInt() : crossSaveOverride;
+		return crossSaveOverride == -1 ? crossSaveOverride = getJE().get("crossSaveOverride").getAsInt() : crossSaveOverride;
 	}
 
+	/**
+	 * Get the applicable membership types declared in the response
+	 */
 	public ArrayList<Integer> getApplicableMembershipTypes() {
 		if (applicableMembershipTypes.isEmpty()) {
-			checkJE();
-			applicableMembershipTypes.clear();
-			for (JsonElement jj : je.get("applicableMembershipTypes").getAsJsonArray()) {
+			for (JsonElement jj : getJE().get("applicableMembershipTypes").getAsJsonArray()) {
 				applicableMembershipTypes.add(jj.getAsInt());
 			}
 		}
 		return applicableMembershipTypes;
 	}
 
+	/**
+	 * Get all membership types declared under this account's profiles
+	 */
+	public ArrayList<Integer> getMembershipTypes() {
+		ArrayList<Integer> integers = new ArrayList<>();
+
+		for(JsonElement jsonElement : getJO().get("profiles").getAsJsonArray()) {
+			integers.add(jsonElement.getAsJsonObject().get("membershipType").getAsInt());
+		}
+
+		return integers;
+	}
+
 	public boolean isPublic() {
-		checkJE();
-		return !isPublic ? isPublic = je.get("isPublic").getAsBoolean() : isPublic;
+		return !isPublic ? isPublic = getJE().get("isPublic").getAsBoolean() : isPublic;
 	}
 
 	/**
-	 * Returns the membership type
+	 * Returns the membership type of the main profile
 	 */
 	public int getMembershipType() {
-		checkJE();
-		return membershipType == -1 ? membershipType = je.get("membershipType").getAsInt() : membershipType;
+		return membershipType == -1 ? membershipType = getJE().get("membershipType").getAsInt() : membershipType;
 	}
 
 	/**
-	 * Gets the characters that are attached to this bungieuser
+	 * Gets the characters that are attached to this BungieUser
+	 *
+	 * @return The list of characters associated with this account, or null if none are found
 	 */
 	public List<DestinyCharacter> getCharacters() {
 		if (destinyCharacters != null) { return destinyCharacters; }
@@ -169,6 +209,9 @@ public class BungieUser {
 		return destinyCharacters;
 	}
 
+	/**
+	 * Adds up all of the time played across all characters
+	 */
 	public int getTimePlayed() {
 		if (playTime != -1) { return playTime; }
 		for (DestinyCharacter c : getCharacters()) {
@@ -183,8 +226,8 @@ public class BungieUser {
 	public Clan getClan() {
 		if (clan != null) { return clan; }
 
-		JsonObject jo = hu.urlRequestGET("https://www.bungie.net/Platform/GroupV2/User/" + getMembershipType() + "/" + getBungieMembershipID() + "/0/1/?components=200").getAsJsonObject("Response");
-		clan = new Clan(jo.get("results").getAsJsonArray().get(0).getAsJsonObject().getAsJsonObject("group").get("groupId").getAsLong());
+		JsonObject jo2 = hu.urlRequestGET("https://www.bungie.net/Platform/GroupV2/User/" + getMembershipType() + "/" + getBungieMembershipID() + "/0/1/?components=200").getAsJsonObject("Response");
+		clan = new Clan(jo2.get("results").getAsJsonArray().get(0).getAsJsonObject().getAsJsonObject("group").get("groupId").getAsLong());
 		return clan;
 	}
 
@@ -200,13 +243,14 @@ public class BungieUser {
 	/**
 	 * Return a lot of information pertaining to the activity the user is currently in
 	 * E.g. If I am on the tower it will provide information about the ActivityMode and Icons related to it
+	 *
 	 * @return Null if no information about the current activity is found
 	 */
 	public ActivityInfo getCurrentActivityInfo() {
 		JsonObject data = hu.urlRequestGET("https://www.bungie.net/Platform/Destiny2/" + getMembershipType() + "/Profile/" + getBungieMembershipID() + "/?components=204").getAsJsonObject("Response").getAsJsonObject("characterActivities").getAsJsonObject("data");
 
-		for(DestinyCharacter character : getCharacters()) {
-			if(data.has(character.getCharacterID())) {
+		for (DestinyCharacter character : getCharacters()) {
+			if (data.has(character.getCharacterID())) {
 				String hash = data.getAsJsonObject(character.getCharacterID()).get("currentActivityHash").getAsString();
 				if (!hash.equals("0")) {
 					return new ActivityInfo(hash);
@@ -224,21 +268,25 @@ public class BungieUser {
 		hu.urlRequestPOSTOauth("https://www.bungie.net/Platform/GroupV2/" + clan.getClanID() + "/Members/Apply/" + getMembershipType() + "/", "");
 	}
 
-	public JsonObject getJsonObject() {
-		checkJO();
-		return jo;
-	}
-
-	public void checkJO() {
-		if (jo == null) {
-			jo = hu.urlRequestGET("https://www.bungie.net/Platform/Destiny2/-1/Profile/" + bungieMembershipID + "/LinkedProfiles/?components=200");
-		}
-	}
-
-	public void checkJE() {
-		checkJO();
+	/**
+	 * Get the profile object to be used to pull most data
+	 * Uses the preffered platform if it has been declared or
+	 * it will select the first in the profiles array
+	 */
+	public JsonObject getJE() {
 		if (je == null) {
-			je = jo.get("Response").getAsJsonObject().get("profiles").getAsJsonArray().get(0).getAsJsonObject();
+			if(intendedPlatform != -2) {
+				for(JsonElement jsonElement : getJO().get("profiles").getAsJsonArray()) {
+					if(jsonElement.getAsJsonObject().get("membershipType").getAsInt() == intendedPlatform) {
+						je = jsonElement.getAsJsonObject();
+						break;
+					}
+				}
+			} else {
+				je = getJO().get("profiles").getAsJsonArray().get(0).getAsJsonObject();
+			}
 		}
+
+		return je;
 	}
 }
---
+++ src/main/java/material/user/DestinyPlatform.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) dec4234 2021. Access is granted, without any express warranties or guarantees of
+ * any kind,  to all wishing to use this software for their benefit. No one may specifically claim credit, or
+ * ownership of this software without the explicit permission of the author.
+ *
+ * GitHub -> https://github.com/dec4234/JavaDestinyAPI
+ */
+
+package material.user;
+
+/**
+ * Information about each platform that Destiny 2 has/is on
+ *
+ * Platform codes and fancy names for each are provided
+ */
+public enum DestinyPlatform {
+
+	NONE(0, "None"),
+
+	XBOX(1, "Xbox"),
+	PSN(2, "PSN"),
+	STEAM(3, "Steam"),
+	BLIZZARD(4, "Blizzard"),
+	STADIA(5, "Stadia"),
+	DEMON(10, "Demon"),
+
+	// Used for searching
+	BUNGIE_NEXT(254, "BungieNext"),
+	ALL(-1, "All");
+
+	private int platformCode;
+	private String fancyName;
+
+	DestinyPlatform(int platformCode, String fancyName) {
+		this.platformCode = platformCode;
+		this.fancyName = fancyName;
+	}
+
+	/**
+	 * Get the plaform code associated with this platform
+	 */
+	public int getPlatformCode() {
+		return platformCode;
+	}
+
+	/**
+	 * Get the fancy name associated with this platofrm e.g. "Steam" instead of "STEAM"
+	 * @return
+	 */
+	public String getFancyName() {
+		return fancyName;
+	}
+
+	/**
+	 * Get a DestinyPlatform from a platform code
+	 */
+	public static DestinyPlatform fromMembershipType(int platformCode) {
+		for(DestinyPlatform destinyPlatform : DestinyPlatform.values()) {
+			if(destinyPlatform.getPlatformCode() == platformCode) {
+				return destinyPlatform;
+			}
+		}
+
+		return null;
+	}
+}
